792ddf906a5f963f2d5324eb9cd997e403d28255
Brian <oyollobrian@gmail.com>
2022-08-17 06:24:36 +0300

    update path

--- movie.sh
+++ movie.sh
@@ -3,9 +3,9 @@
 #This is a simple script to let me know when Top Gun: Maverick(or any other movie in the future) will be available for streaming.
 #It is scheduled to run everyday at 23.00 (0 23 * * *). 
 
-movie="Top Gun: Maverick"
+movie="Thor: Love and Thunder"
 count=$(curl -l -o movie.txt https://tfpdl.se/category/movies/ | grep -i $movie movie.txt | wc -l)
 if [ $count -gt 1 ]; then 
-        echo $movie" is available for streaming." > /home/brian/Desktop/$movie.txt
+        echo $movie" is available for streaming." > ~/Desktop/"$movie".txt
 fi
 
