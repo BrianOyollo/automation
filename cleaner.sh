236bfa560d4ebcff65aacd175a91b756cc361c32
Oyollo <oyollobrian@gmail.com>
2022-08-27 17:11:33 +0300

    cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -4,17 +4,23 @@
 for file in ~/Downloads/*
 do  
     case $file in
-        *.pdf | *.docx)
-            mkdir -p ~/Desktop/trial/Docs/$(date +'%B%Y') && cp "$file" ~/Desktop/trial/Docs/$(date +'%B%Y');;
+        *.txt | *.docx | *.doc | *.csv | *.tsv)
+            mkdir -p ~/Documents/$(date +'%B%Y') && cp "$file" ~/Documents/$(date +'%B%Y');;
         
         *.torrent)
-            mkdir -p ~/Desktop/trial/Torrents/$(date +'%B%Y') && cp "$file" ~/Desktop/trial/Torrents/$(date +'%B%Y');;
+            mkdir -p ~/Documents/Torrents/$(date +'%B%Y') && cp "$file" ~/Documents/Torrents/$(date +'%B%Y');;
         
         *.jpeg | *.jpg | *.png | *.webp)
-            mkdir -p ~/Desktop/trial/Pictures/$(date +'%B%Y') && cp "$file" ~/Desktop/trial/Pictures/$(date +'%B%Y');;
+            mkdir -p ~/Pictures/$(date +'%B%Y') && cp "$file" ~/Pictures/$(date +'%B%Y');;
 
         *.mp4 | *.mkv | *.avi )
-            mkdir -p ~/Desktop/trial/Videos/$(date +'%B%Y') && cp "$file" ~/Desktop/trial/Videos/$(date +'%B%Y');;
+            mkdir -p ~/Videos/$(date +'%B%Y') && cp "$file" ~/Videos/$(date +'%B%Y');;
+	
+	*.mp3 | .m4a | *.wav | *.wma)
+	    mkdir -p ~/Music/$(date +'%B%Y') && cp "$file" ~/Music/$(date +'%B%Y');;
+
+	*.zip | *.rar)
+	    mkdir -p ~/Downloads/zips/$(date +"%B%Y") && cp "$file" ~/Downloads/zips/$(date +"%B%Y")
 
 
 
