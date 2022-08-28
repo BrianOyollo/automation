3d076c39fbb7641dff18bb6dc0ee06822ca90a7b
Oyollo <oyollobrian@gmail.com>
2022-08-28 13:12:40 +0300

    cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -1,26 +1,28 @@
-# script to clear downloads folder on monthly basis.
+#!/bin/bash
+# script to clear my downloads folder.
+# scheduled to run daily at midnight.
 
 
 for file in ~/Downloads/*
 do  
     case $file in
-        *.txt | *.docx | *.doc | *.csv | *.tsv)
-            mkdir -p ~/Documents/$(date +'%B%Y') && cp "$file" ~/Documents/$(date +'%B%Y');;
+        *.txt | *.docx | *.doc | *.csv | *.tsv | *.pdf | *.DOC | *.pptx | *.xlsx)
+            mkdir -p ~/Documents/$(date +'%B%Y') && mv "$file" ~/Documents/$(date +'%B%Y');;
         
         *.torrent)
-            mkdir -p ~/Documents/Torrents/$(date +'%B%Y') && cp "$file" ~/Documents/Torrents/$(date +'%B%Y');;
+            mkdir -p ~/Documents/Torrents/$(date +'%B%Y') && mv "$file" ~/Documents/Torrents/$(date +'%B%Y');;
         
         *.jpeg | *.jpg | *.png | *.webp)
-            mkdir -p ~/Pictures/$(date +'%B%Y') && cp "$file" ~/Pictures/$(date +'%B%Y');;
+            mkdir -p ~/Pictures/$(date +'%B%Y') && mv "$file" ~/Pictures/$(date +'%B%Y');;
 
         *.mp4 | *.mkv | *.avi )
-            mkdir -p ~/Videos/$(date +'%B%Y') && cp "$file" ~/Videos/$(date +'%B%Y');;
+            mkdir -p ~/Videos/$(date +'%B%Y') && mv "$file" ~/Videos/$(date +'%B%Y');;
 	
 	*.mp3 | .m4a | *.wav | *.wma)
-	    mkdir -p ~/Music/$(date +'%B%Y') && cp "$file" ~/Music/$(date +'%B%Y');;
+	    mkdir -p ~/Music/$(date +'%B%Y') && mv "$file" ~/Music/$(date +'%B%Y');;
 
-	*.zip | *.rar)
-	    mkdir -p ~/Downloads/zips/$(date +"%B%Y") && cp "$file" ~/Downloads/zips/$(date +"%B%Y")
+	*.zip | *.rar | *.gz)
+	    mkdir -p ~/Downloads/zips/$(date +"%B%Y") && mv "$file" ~/Downloads/zips/$(date +"%B%Y")
 
 
 
